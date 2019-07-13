c678e058470548e63e173ff3ce95c5dab11aaaba
kemengjian <kemengjian@126.com>
2019-07-13 13:34:22 +0800

    feat: 清除内嵌式数据库

--- src/main/java/com/study/tdd/TddApplication.java
+++ src/main/java/com/study/tdd/TddApplication.java
@@ -2,11 +2,8 @@ package com.study.tdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class TddApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/study/tdd/ums_admin/repository/UmsAdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.study.tdd.ums_admin.repository;
-
-import com.study.tdd.ums_admin.entity.UmsAdmin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UmsAdminRepository extends JpaRepository<UmsAdmin,Long> {
-}
